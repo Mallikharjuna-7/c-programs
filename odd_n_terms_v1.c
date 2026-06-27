1ee4c48e1e8c531b6e9638fc5172ed6b4311019b
Mallikharjuna-7 <mallikharjuna.xworkz@gmail.com>
2026-06-27 10:07:14 +0530

    n odd numbers v1

---
+++ odd_n_terms_v1.c
@@ -0,0 +1,43 @@
+/*
+
+step-1: start
+step-2: print "How many odd numbers do you want to print? "
+step-3: read OddCount
+step-4: print "The first " + OddCount + " odd numbers are "
+step-5: Odd = 1
+step-6: print Odd
+step-7: OddCounter = 1
+step-8: if(Oddcounter < OddCount):
+          Odd = Odd + 2
+          print ", " + Odd
+          OddCounter = OddCounter + 1
+          goto ste-8
+step-9: print "./n"
+step-10: stop
+
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+
+  int odd_count, odd, odd_counter;
+
+  printf("How many odd numbers do you want to print? ");
+  scanf("%i", &odd_count);
+  printf("The first %i  odd numbers are ", odd_count);
+  odd = 1;
+  printf("%i", odd);
+  odd_counter = 1;
+  while(odd_counter < odd_count)
+  {
+    odd = odd + 2;
+    printf(", %i", odd);
+    odd_counter = odd_counter + 1;
+  }
+  printf(".\n");
+
+  return 0;
+
+}
